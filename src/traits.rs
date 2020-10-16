f7e71fa9476f0e71d76adf97ccb536fe0bcf8396
Vincent Junge <jungevincent@gmail.com>
2020-10-16 02:18:07 +0200

    Remove Default from ObjectID

--- src/traits.rs
+++ src/traits.rs
@@ -4,13 +4,13 @@ use std::fmt::Debug;
 use std::hash::Hash;
 
 #[cfg(not(feature="parallel"))]
-pub trait ObjectID: Copy + Clone + Default + Hash + Ord + Debug {}
+pub trait ObjectID: Copy + Clone + Hash + Ord + Debug {}
 
 #[cfg(not(feature="parallel"))]
-impl<T: Copy + Clone + Default + Hash + Ord + Debug> ObjectID for T {}
+impl<T: Copy + Clone + Hash + Ord + Debug> ObjectID for T {}
 
 #[cfg(feature="parallel")]
-pub trait ObjectID: Copy + Clone + Default + Hash + Ord + Send + Sync + Debug {}
+pub trait ObjectID: Copy + Clone + Hash + Ord + Send + Sync + Debug {}
 
 #[cfg(feature="parallel")]
-impl<T: Copy + Clone + Default + Hash + Ord + Send + Sync + Debug> ObjectID for T {}
+impl<T: Copy + Clone + Hash + Ord + Send + Sync + Debug> ObjectID for T {}
